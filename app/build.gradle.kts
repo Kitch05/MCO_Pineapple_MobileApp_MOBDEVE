e3843e3134baa97bb7e186b602ab2d0d8a74b461
Ken Cheng <87518459+Kitch05@users.noreply.github.com>
2024-10-19 00:39:18 +0800

    layout for profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(libs.recyclerview)
+    implementation (libs.material.v120)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
