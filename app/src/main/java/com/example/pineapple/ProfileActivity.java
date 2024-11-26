483a72761212a02b031dc3d800cdd04a19638bd5
Ken Cheng <87518459+Kitch05@users.noreply.github.com>
2024-11-27 02:04:51 +0800

    fixed bug with remember me and sign out

--- app/src/main/java/com/example/pineapple/ProfileActivity.java
+++ app/src/main/java/com/example/pineapple/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.pineapple;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -57,6 +58,11 @@ public class ProfileActivity extends BaseActivity {
         signoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences sharedPreferences = sharedPreferences = getSharedPreferences("PineapplePrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear(); // This removes all stored preferences
+                editor.apply();
+
                 FirebaseAuth mAuth = FirebaseAuth.getInstance();
                 mAuth.signOut();
 
